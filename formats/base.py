ac6eeb7b10b46464b6901c6f7e730e50aa5eef98
vbamnup <vbamnup@gmail.com>
2025-12-21 19:44:04 +0300

    Add base class book parser

---
+++ formats/base.py
@@ -0,0 +1,40 @@
+from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import Iterator
+
+
+class BaseBookParser(ABC):
+    """Базовый класс парсера книг"""
+
+    def __init__(self, book_path: Path):
+        self.book_path = book_path
+
+        if not self.book_path.exists():
+            raise ValueError(f"Книга {book_path} не найдена")
+
+    @abstractmethod
+    def parse(self) -> Iterator['Chapter']:
+        """
+        Парсит книгу и возвращает итератор глав
+
+        Returns:
+            Итератор глав
+        """
+
+        raise NotImplementedError
+
+    @classmethod
+    @abstractmethod
+    def supports(cls, book_path: Path) -> bool:
+        """
+        Проверяет, поддерживается ли данный формат
+
+        Args:
+            book_path (Path): Путь к книге
+
+        Returns:
+            True, если поддерживается
+        """
+
+        raise NotImplementedError
+
